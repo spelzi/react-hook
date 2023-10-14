290e217f0662d39ae48affaef67b643bc7041231
spelzi <60978130+spelzi@users.noreply.github.com>
2023-10-14 15:15:39 +0100

    moviecard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import MovieList from './compnonet/MovieList';
+import Filter from './compnonet/Filter';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [movies, setMovies] = useState([
+  // you can add move movies here 
+    { title: 'The return of the Gods', description: 'A local movis telling a deep tell of the pass', posterURL: 'https://hips.hearstapps.com/hmg-prod/images/best-movies-1606255499.jpg?crop=0.888888888888889xw:1xh;center,top&resize=1200:*', rating: 4.5 },
+    { title: 'Adems Book ', description: 'This is a story tell a historias passed of a world lost to the sand storm', posterURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9im_C8W_kOISgRkPt0eb2L4Rj0miXh3h1DuDVFMJ8QaJB3YV-Hhn7MQSo0HBU9CPaQ8o&usqp=CAU', rating: 3.7 },
+    { title: 'The return of the Gods', description: 'A local movis telling a deep tell of the pass', posterURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLIYlsd_ZesDX03AEPdaDX5EQDzZuM-ssIsw&usqp=CAU', rating: 4.5 },
+    { title: 'Adems Book ', description: 'This is a story tell a historias passed of a world lost to the sand storm', posterURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9im_C8W_kOISgRkPt0eb2L4Rj0miXh3h1DuDVFMJ8QaJB3YV-Hhn7MQSo0HBU9CPaQ8o&usqp=CAU', rating: 3.7 },
+    
+  ]);
+
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
+  const filterTitle = (title) => {
+    const filtered = movies.filter((movie) => movie.title.includes(title));
+    setFilteredMovies(filtered);
+  };
+
+  const filterRating = (rating) => {
+    const filtered = movies.filter((movie) => movie.rating >= parseFloat(rating));
+    setFilteredMovies(filtered);
+  };
+
   return (
+    // this is the web display content
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie List</h1>
+      <Filter onFilterTitle={filterTitle} onFilterRating={filterRating} />
+      <MovieList movies={filteredMovies}  />
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/compnonet/MovieCard.js
@@ -0,0 +1,20 @@
+// MovieCard.js
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const MovieCard = ({ title, description, posterURL, rating }) => {
+  return (
+    // this is a react-bootstrap card
+    <Card style={{ width: '100%', marginTop: '50px' }}>
+      <Card.Img variant="top" src={posterURL} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Card.Text>Rating: {rating}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default MovieCard;
+
