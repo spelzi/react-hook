d5b07d7d8d6d5ff1dcfbaac4d5877c70bb37fba1
spelzi <60978130+spelzi@users.noreply.github.com>
2023-10-20 12:05:03 +0100

    Movie Page

--- src/compnonet/MovieCard.js
+++ src/compnonet/MovieCard.js
@@ -1,20 +1,21 @@
 // MovieCard.js
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Card } from "react-bootstrap";
 
 const MovieCard = ({ title, description, posterURL, rating }) => {
   return (
     // this is a react-bootstrap card
-    <Card style={{ width: '100%', marginTop: '50px' }}>
-      <Card.Img variant="top" src={posterURL} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Card.Text>Rating: {rating}</Card.Text>
-      </Card.Body>
-    </Card>
+    <a href={`/movies/${title}`}>
+      <Card style={{ width: "100%", marginTop: "50px" }}>
+        <Card.Img variant="top" src={posterURL} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <Card.Text>Rating: {rating}</Card.Text>
+        </Card.Body>
+      </Card>
+    </a>
   );
 };
 
 export default MovieCard;
-
